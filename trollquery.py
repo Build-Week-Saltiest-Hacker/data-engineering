178e90bcd5a11b354e968f9e31ea37fa557ede82
jesse berkowitz <jberkow@brandeis.edu>
2020-05-26 16:46:51 -0400

    queries for trolltrace

---
+++ trollquery.py
@@ -0,0 +1,44 @@
+import sqlite3
+import pandas as pd 
+import os
+import json
+from dotenv import load_dotenv 
+import psycopg2
+from psycopg2.extras import execute_values
+import sqlite3
+import numpy as np 
+
+connection = sqlite3.connect("trolldb.sqlite3")
+cursor = connection.cursor()
+
+
+q1 = '''select t.text , t.by, t.neg_score, t.vader_score
+from troll_table as t 
+order by t.vader_score DESC
+limit 10;'''
+
+r1 = cursor.execute(q1)
+
+print( r1.fetchall())
+
+q2 = ''' select t.by, t.neg_score, t.vader_score, t.text
+from troll_table as t 
+where t.neg_score > .2 AND t.vader_score < -.6
+order by t.neg_score DESC;'''
+
+
+r2= cursor.execute(q2)
+print(r2.fetchall())
+
+q3 = '''select avg(t.neg_score), avg(t.vader_score) from troll_table as t  
+where t.by = 'meddlepal';'''
+
+r3 = cursor.execute(q3)
+print(r3.fetchall())
+
+q4 = '''select t.text   from troll_table as t  
+where t.by = 'meddlepal';'''
+r4 = cursor.execute(q4)
+print(r4.fetchall())
+
+

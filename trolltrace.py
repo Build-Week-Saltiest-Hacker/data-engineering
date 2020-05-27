7a5a6b9e8576d8bcb5ad6dc1ed41c0a5166b9f36
jesse berkowitz <jberkow@brandeis.edu>
2020-05-26 20:40:46 -0400

    trolltrace 2.0

--- trolltrace.py
+++ trolltrace.py
@@ -12,20 +12,24 @@ class Troll(db.Model):
     neg_score = db.Column(db.Integer, nullable=False)
 
     def high_troll(self):
-        db_text = text.query.all 
-        db_vader_score = vader_score.query.all 
+        db_vader_score = text.query.filter_by(vader_score = max)
+        return db_vader_score(0-10) 
         #find and return 10 texts where vader score is highest
 
     def user_vader(x):
-        db_user = user.query.all
+        uservader = vader_score.query.filter_by(user='x')
         #find and return avg vader score for user x
+        return uservader
          
     def user_neg(x):
-        db_neg_score = neg_score.query.all
+        db_neg_score = neg_score.query.filter_by(user='x')
         #find and return avg neg_score for user x     
-         
+        return db_neg_score 
+
     def user_text(x):
-        #search through all texts, return all texts for user(x)    
+        #search through all texts, return all texts for user(x)  
+        db_user_text = text.query.filter_by(user='x')
+        return all db_user_text    
               
         
           
